148dc48913b7213287eae01b59ff8cfe37d94318
David Carvalho <david.carvalho@ocado.com>
2017-12-14 16:36:16 +0000

    Starting Array exercise.

---
+++ Array/Array/Array.h
@@ -0,0 +1,19 @@
+//
+//  Array.h
+//  Array
+//
+//  Created by David Carvalho on 14/12/2017.
+//  Copyright © 2017 David Carvalho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Array.
+FOUNDATION_EXPORT double ArrayVersionNumber;
+
+//! Project version string for Array.
+FOUNDATION_EXPORT const unsigned char ArrayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Array/PublicHeader.h>
+
+
