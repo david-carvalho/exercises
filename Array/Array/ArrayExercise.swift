a819d66e895d36cafe340b7684a6c23a72d0122a
David Carvalho <david.carvalho@ocado.com>
2017-12-16 20:38:22 +0000

    Added what I believe is correct memory management. It is now possible to increase the capacity of the array Added some access and search speed tests.

--- Array/Array/ArrayExercise.swift
+++ Array/Array/ArrayExercise.swift
@@ -6,26 +6,47 @@ class ArrayExercise<T> {
     private var capacity: Int
     
     init(capacity: Int = 10) {
-        // TODO: deallocate this
+        // TODO: Any kind of thread operations will very easily break
         self.capacity = capacity
         buffer = UnsafeMutablePointer<T>.allocate(capacity: capacity)
     }
     
+    deinit {
+        // TODO: Investigate if this is enough
+        buffer.deinitialize(count: count)
+        buffer.deallocate(capacity: capacity)
+    }
+    
     func append(object: T) {
         if count >= capacity {
-            let tempCapacity = capacity * 2
-            let tempBuffer = UnsafeMutablePointer<T>.allocate(capacity: tempCapacity)
-            for counter in 0...count {
-                let object: T = buffer.advanced(by: counter).pointee
-                tempBuffer.advanced(by: counter).initialize(to: object)
-                buffer.advanced(by: counter).deinitialize()
-            }
-            buffer.deallocate(capacity: capacity)
-            buffer = tempBuffer
-            capacity = tempCapacity
+            increaseArrayCapacity()
         }
         buffer.advanced(by: count).initialize(to: object)
         count += 1
+    }
+    
+    private func increaseArrayCapacity() {
+        let tempCapacity = capacity * 2
+        let tempBuffer = UnsafeMutablePointer<T>.allocate(capacity: tempCapacity)
+        for counter in 0..<count {
+            let object: T = buffer.advanced(by: counter).pointee
+            tempBuffer.advanced(by: counter).initialize(to: object)
+        }
+        buffer.deinitialize(count: capacity)
+        buffer.deallocate(capacity: capacity)
+        buffer = tempBuffer
+        capacity = tempCapacity
+    }
+}
+
+extension ArrayExercise where T: Equatable {
+    func contains(_ object: T) -> Bool {
+        for counter in 0..<count {
+            if object == buffer.advanced(by: counter).pointee {
+                return true
+            }
+        }
+        return false
     }
 }
 
@@ -33,19 +54,20 @@ extension ArrayExercise: Collection {
     func index(after i: Int) -> Int {
         return 0
     }
-
+    
     var startIndex: Int {
         return 0
     }
-
+    
     var endIndex: Int {
         return count
     }
-
+    
     subscript(i: Int) -> T {
         get {
-            return buffer[i]
+            return buffer.advanced(by: i).pointee
         }
     }
 }
+
 
--- Array/ArrayTests/ArrayExerciseTests.swift
+++ Array/ArrayTests/ArrayExerciseTests.swift
@@ -4,6 +4,17 @@ import XCTest
 
 class ArrayExerciseTests: XCTestCase {
     
+    var runSpeedTests = false
+    let numberElementsForSpeedRuns = [10_000,
+                                      20_000,
+                                      30_000,
+                                      40_000]
+    
+    override func setUp() {
+        super.setUp()
+        runSpeedTests = true
+    }
+    
     func testArray_AddOneElement_CountIsOne() {
         let array = ArrayExercise<String>()
         array.append(object: "test")
@@ -32,5 +43,59 @@ class ArrayExerciseTests: XCTestCase {
         array.append(object: "test2")
         array.append(object: "test3")
         XCTAssert(array[2] == "test3")
+    }
+    
+    func testAccess() {
+        if runSpeedTests {
+            for numberElements in numberElementsForSpeedRuns {
+                runAccess(for: numberElements)
+            }
+        }
+    }
+    
+    private func runAccess(for numberElements: Int) {
+        let array = createAndFillArray(for: numberElements)
+        countTime(for: numberElements, array: array) { (array, currentElements, index) in
+            _ = array[index]
+        }
+    }
+    
+    func testSearch() {
+        if runSpeedTests {
+            for numberElements in numberElementsForSpeedRuns {
+                runSearch(for: numberElements)
+            }
+        }
+    }
+    
+    private func runSearch(for numberElements: Int) {
+        let array = createAndFillArray(for: numberElements)
+    
+        countTime(for: numberElements, array: array) { (array, currentElement, index) in
+            _ = array.contains(currentElement)
+        }
+    }
+    
+    private func countTime(for numberElements: Int,
+                           array: ArrayExercise<String>,
+                           operation: ((ArrayExercise<String>, String, Int) -> Void)) {
+        var totalTime: Double = 0
+        
+        for counter in 0..<numberElements {
+            let startDate = Date()
+            operation(array, array[counter], counter)
+            
+            totalTime += Date().timeIntervalSince(startDate)
+        }
+        
+        print("For \(numberElements) it took \(totalTime / Double(numberElements)) ms")
+    }
+    
+    private func createAndFillArray(for numberElements: Int) -> ArrayExercise<String> {
+        let array = ArrayExercise<String>()
+        for counter in 0...numberElements {
+            array.append(object: String(counter))
+        }
+        return array
     }
 }
